290bbdd87a3b297de4f62d5e1ce8b9d564711884
Anıl Uzunoğlu <aniluzunoglu@hotmail.com>
2022-09-20 23:25:45 +0300

    Resource Management v0.1
    Added data persistence and scene management.

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -26,7 +26,10 @@ public abstract class Unit : MonoBehaviour,
 
     private void Start()
     {
-
+        if (MainManager.Instance != null)
+        {
+            SetColor(MainManager.Instance.ChosenColor);
+        }
     }
 
     void SetColor(Color c)
